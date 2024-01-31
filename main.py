cf1eb20ff2252cce7440b8181e7b05af4647bdaf
lichuuuuuu <117422790+lichuuuuuu@users.noreply.github.com>
2024-01-31 22:59:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 # 最新の数値と受信時間を保存するリスト
 latest_numbers = []
+congestion_levels = []
 
 # 数値を追加するエンドポイント
 @route('/add', method='POST')
@@ -17,7 +18,22 @@ def add_number():
 # 数値を表示するエンドポイント
 @route('/numbers')
 def show_numbers():
+    global congestion_levels
     latest_date = datetime.now().strftime("%Y-%m-%d") if latest_numbers else "データなし"
-    return template('template.html', numbers=latest_numbers, latest_date=latest_date)
+    
+    # 平均距離と背景色、混雑度を計算
+    if latest_numbers:
+        average_distance = sum(distance for distance, _ in latest_numbers) / len(latest_numbers)
+        congestion_level_value = round(average_distance * 100 / 1000, 1)
+        congestion_level = f"{congestion_level_value}"
+        congestion_levels.append([congestion_level, latest_date])
+        
+    else:
+        congestion_level = "データなし"
 
-run(host="0.0.0.0", port=8080, debug=True)
+    if len(congestion_levels) > 10:
+        congestion_levels = congestion_levels[-10:]
+
+    return template('template.html', numbers=latest_numbers, latest_date=latest_date, congestion_level=congestion_level, congestion_levels=congestion_levels)
+
+run(host="192.168.45.8", port=8080, debug=True)
